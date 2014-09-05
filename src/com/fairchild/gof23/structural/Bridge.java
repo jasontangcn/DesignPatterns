a82f3535f03bef0bd965d2e9942aef0a3ae42edc
Jason Tang <TomHornson@hotmail.com>
2014-09-05 08:52:02 +0800

    + Bridge pattern.

--- src/com/fairchild/gof23/structural/Bridge.java
+++ src/com/fairchild/gof23/structural/Bridge.java
@@ -6,8 +6,91 @@ package com.fairchild.gof23.structural;
 
 /**
  * @author TomHornson(at)hotmail.com
+ * 
+ * Separate behaviors and abstraction. 
+ * Difference between Bridge and Abstract Factory.
  */
 public class Bridge {
-	public static void main(String[] args) {
+	public static class Point {
+		private int x;
+		private int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+	}
+
+	public static interface Drawer {
+		public void drawLine(Point start, Point end);
+	}
+
+	public static class WinDrawer implements Drawer {
+		public void drawLine(Point start, Point end) {
+			System.out.println("WinDrawer");
+		}
+	}
+
+	public static class UnixDrawer implements Drawer {
+		public void drawLine(Point start, Point end) {
+			System.out.println("UnixDrawer");
+		}
+	}
+
+	public static class DrawerFactory {
+		private static Drawer drawer = null;
+
+		public synchronized static Drawer getInstance() {
+			boolean isWinSystem = true;
+
+			if (null == drawer) {
+				if (isWinSystem) {
+					drawer = new WinDrawer();
+				} else {
+					drawer = new UnixDrawer();
+				}
+			}
+			return drawer;
+
+		}
+	}
+
+	public abstract static class Shape {
+		Drawer drawer = DrawerFactory.getInstance();
+
+		public abstract void draw();
+	}
+
+	public static class Triangle extends Shape {
+		public void draw() {
+			drawer.drawLine(new Point(0, 0), new Point(10, 0));
+			drawer.drawLine(new Point(0, 0), new Point(5, 10));
+			drawer.drawLine(new Point(5, 10), new Point(10, 0));
+		}
+	}
+
+	public static class Square extends Shape {
+		public void draw() {
+			drawer.drawLine(new Point(0, 0), new Point(10, 0));
+			drawer.drawLine(new Point(0, 0), new Point(0, 10));
+			drawer.drawLine(new Point(0, 10), new Point(10, 10));
+			drawer.drawLine(new Point(10, 0), new Point(10, 10));
+		}
 	}
 }
--- src/com/fairchild/gof23/structural/Composite.java
+++ src/com/fairchild/gof23/structural/Composite.java
@@ -15,85 +15,85 @@ import java.util.List;
 public class Composite {
 	public static abstract class Device {
 		private String name;
+
 		public Device(String name) {
 			this.name = name;
 		}
-		
+
 		public abstract float getPrice();
-		
-		public boolean addDevice(Device child){
+
+		public boolean addDevice(Device child) {
 			return false;
 		}
-		
+
 		public boolean removeDevice(Device child) {
 			return false;
 		}
-		
+
 		public Iterator iterator() {
 			return null;
 		}
 	}
-	
+
 	public static class MotherBoard extends Device {
 		private List<Device> children = new ArrayList<Device>();
-		
+
 		public MotherBoard(String name) {
 			super(name);
 		}
-		
+
 		public float getPrice() {
 			float price = 0;
-			for(Device device : children) {
+			for (Device device : children) {
 				price += device.getPrice();
 			}
-			
+
 			return price;
 		}
-		
-		
-		public boolean addDevice(Device child){
+
+		public boolean addDevice(Device child) {
 			return children.add(child);
 		}
-		
+
 		public boolean removeDevice(Device child) {
 			return children.remove(child);
 		}
-		
+
 		public Iterator iterator() {
 			return children.iterator();
 		}
 	}
-	
+
 	public static class Memory extends Device {
 		public Memory(String name) {
 			super(name);
 		}
-		
+
 		public float getPrice() {
 			return 200;
 		}
 	}
-	
+
 	public static class Disk extends Device {
 		public Disk(String name) {
 			super(name);
 		}
-		
+
 		public float getPrice() {
 			return 500;
 		}
 	}
-	
+
 	public static class DisplayCard extends Device {
 		public DisplayCard(String name) {
 			super(name);
 		}
-		
+
 		public float getPrice() {
 			return 800;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		DisplayCard displayCard = new DisplayCard("Nvidia");
 		Disk disk = new Disk("Segate");
